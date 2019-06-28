d32f056ee84d5a572f59ae81e8a8ca2a47efe440
Seo Boseok <ggy7bbo@gmail.com>
2019-06-28 13:47:33 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 import './App.css';
 
 function App() {
-  const name = 'ggybbo';
-  const style = {
-    backgroundColor: 'green',
-    color: 'aqua',
-    fontSize: 18,
-    padding: '1rem'
-  }
   return (
     <>
-      {/** 주석활용법 JSX */}
-      <div className="" style={style}>{name}</div>
-      <div className="gray-box"></div>
-      <Hello
-      // 주석 inline
-      />
+      <Wrapper>
+        <Hello name="react" color="green" />
+        <Hello color="pink" />
+      </Wrapper>
     </>
   );
 }
--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 // 함수형태로 컴포넌트를 만들자
-function Hello() {
-  return <div>Hello World</div>
+function Hello({ color, name }) {
+  return <div style={{ color }}>Hello World {name}</div>
+}
+
+// props 기본값 설정
+Hello.defaultProps = {
+  name: 'No Name'
 }
 
 export default Hello;
