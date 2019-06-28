f1247f12c384054f2d9633b833098ba6930f67f0
Seo Boseok <ggy7bbo@gmail.com>
2019-06-28 17:42:29 +0900

    useRef로 DOM 선택 및 컨트롤

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: '',
     nickname: '',
   });
+  const nameInput = useRef();
   const { name, nickname } = inputs;
   // e = event 객체
   const onChanged = (e) => {
     const { name, value } = e.target;
-
+    // 객체상태를 업데이트시 불변성을 지키면서 업데이트 위해서 기존 객체를 복사후 할당
     setInputs({
       ...inputs,
       [name]: value
@@ -19,11 +20,12 @@ function InputSample() {
     setInputs({
       name: '',
       nickname: ''
-    })
+    });
+    nameInput.current.focus();
   }
   return (
     <div>
-      <input name="name" onChange={onChanged} placeholder="name" value={name} />
+      <input name="name" onChange={onChanged} placeholder="name" value={name} ref={nameInput} />
       <input name="nickname" onChange={onChanged} placeholder="nickname" value={nickname} />
       <button onClick={onReset}>초기화</button>
       <div>
